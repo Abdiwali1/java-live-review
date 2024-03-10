6391066e6e4e46b535acb69b787bd9845a3caa80
Cydeo-Saim <saim@cybertekschool.com>
2024-03-10 14:01:01 -0500

    week 1 review

---
+++ src/week2/DreamList.java
@@ -0,0 +1,18 @@
+package week2;
+
+public class DreamList {
+    public static void main(String[] args) {
+
+        String item1 = "Huge TV", item2 = "Vacation\\long break", item3 = "House";
+        System.out.println("List:" +
+                "\n1)\"" + item1 + "\"" +
+                "\n2)\"" + item2 + "\"" +
+                "\n3)\"" + item3 + "\""
+        );
+
+    }
+}
+
+/*
+create a list of items you will buy after you get a job
+ */
--- src/week2/Headphones.java
+++ src/week2/Headphones.java
@@ -13,6 +13,14 @@ public class Headphones {
 
         System.out.println("New headphones brought to you by " + brand + "\nThese " + color + " headphone are only priced at $" + price + "\nOther features include wireless: " + isWireless + ", has warranty: " + hasWarranty + " and a cable length of " + cableLength + "\"\nStart listening now with the battery at " + batteryLevel + "% out of the box");
 
+        var Public = "hello";
+        var age = 10;
+        var num = 0123;
+        var num2 = "0123";
+        System.out.println(num);
+        System.out.println(num2);
+
+
     }
 
 }
