feba4e7f599c8bb41788ff803af02cb785778133
Cydeo-Saim <saim@cybertekschool.com>
2024-03-10 12:10:33 -0500

    week 1 review

---
+++ src/week2/Headphones.java
@@ -0,0 +1,29 @@
+package week2;
+
+public class Headphones {
+    public static void main(String[] args) {
+
+        String brand = "Beats";
+        boolean isWireless = true;
+        byte batteryLevel = 80;
+        double price = 299.99;
+        boolean hasWarranty = false;
+        String color = "Red";
+        int cableLength = 12;
+
+        System.out.println("New headphones brought to you by " + brand + "\nThese " + color + " headphone are only priced at $" + price + "\nOther features include wireless: " + isWireless + ", has warranty: " + hasWarranty + " and a cable length of " + cableLength + "\"\nStart listening now with the battery at " + batteryLevel + "% out of the box");
+
+    }
+
+}
+    /*
+    Headphones [variables, datatypes, printing]
+
+	create a class Headphones
+	add a main method
+
+	declare and assign these variables:
+		brand, is wireless, battery level, price, warranty, color, cable length
+
+	print all variables
+     */
---
+++ src/week2/VariableExample.java
@@ -0,0 +1,20 @@
+package week2;
+
+public class VariableExample {
+    public static void main(String[] args) {
+
+        int one = 40;
+        long two = 50;
+
+//        long three = 3_000_000_000;   this does not compile because 3 billion is too big for an int to hold. int is the default type for whole number
+        long three = 3_000_000_000L;
+
+//        float price = 12.99;  12.99 is double. double cannot be stored into a float automatically. double is bigger than float, so we tell the compiler this is not double it is a float
+
+        float price = 12.99F;
+
+        float price2 = 10; // 10 is int, int is smaller than float, so no problem
+        System.out.println(price2);
+
+    }
+}
