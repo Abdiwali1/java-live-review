29930f2f4f87cf933fe4c4c79ebb4f24377f61b5
Cydeo-Saim <saim@cybertekschool.com>
2024-03-24 12:02:40 -0500

    week 4 class

---
+++ src/week4/MultiplicationTable.java
@@ -0,0 +1,34 @@
+package week4;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        multiTableFor(6);
+    }
+
+    public static void multiTableFor(int n){
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+
+}
+
+/*
+Multiplication Table [methods, loops, operators]
+
+	create a method that will accept a number. Print the multiplication table for the give number up until 10
+
+	print the results in this format:
+		$number x 1 = result
+		$number x 2 = result
+		...
+		$number x 10 = result
+
+		ex:
+			number = 4
+			4 x 1 = 4
+			4 x 2 = 8
+			...
+
+ */
